670fce467cf9579cfbd98bf688f5ac8ffcda689a
Christine Zosche <christine.zosche@gmail.com>
2020-04-24 18:51:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   lowest_key = nil
   name_hash.each do |key, value|
-    if value == lowest_value || value < lowest_value
-    lowest_value = value
-    lowest_key = key
+    if lowest_value == nil || value < lowest_value
+    value = lowest_value
+    key = lowest_key
     end
   end
   lowest_key
