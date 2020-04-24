d7b59ce46fa133453b202dbd7e151f9a51b53c7c
Christine Zosche <christine.zosche@gmail.com>
2020-04-24 18:23:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,9 @@ def key_for_min_value(name_hash)
   new_hash = {}
   name_hash.each do |key, value|
     if value < new_hash[value]
-      new_hash[value]
-      
-
+      new_hash.pop
+      new_hash[key] = value
+    end
+  end
+  new_hash[key]   
 end
